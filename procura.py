e9220dae2c4d0641fc1839d83d6874fa7963fa93
Natanael Antonioli <fabricadenoobs2@gmail.com>
2021-08-17 15:10:09 -0300

    Add files via upload

--- procura.py
+++ procura.py
@@ -2,6 +2,7 @@
 
 contador_presente = 0
 contador_ausente = 0
+AUSENTES = []
 
 with open('Adreno.txt', 'r') as file:
         # reading each line
@@ -15,8 +16,13 @@ with open('Adreno.txt', 'r') as file:
                 if word.replace(",","") in open('hacka.txt').read():
                     contador_presente = contador_presente + 1;
                 else:
-                    contador_ausente = contador_ausente + 1;
-                    print(word)
+                    if word in AUSENTES:
+                        pass
+                    else:
+                        AUSENTES.append(word)
+                        contador_ausente = contador_ausente + 1;
+                        print(word)
+
 
 print("O total de palavras presentes é: " + str(contador_presente))
 print("O total de palavras ausentes é: " + str(contador_ausente) + " e elas estão acima.")
